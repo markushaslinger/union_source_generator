e469cf2f917435be4d942953a59a88a45d419dcf
Markus <markus@haslinger.it>
2024-03-23 14:46:13 +0100

    added some default types

---
+++ UnionGen.Types/Types.cs
@@ -0,0 +1,51 @@
+﻿namespace UnionGen.Types;
+
+public readonly struct Yes;
+
+public readonly struct No;
+
+public readonly struct Unknown;
+
+public readonly struct True;
+
+public readonly struct False;
+
+public readonly struct All;
+
+public readonly struct Some;
+
+public readonly struct None;
+
+public readonly struct NotFound;
+
+public readonly struct Success<T>
+{
+    public Success(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; }
+}
+
+public readonly struct Result<T>
+{
+    public Result(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; }
+}
+
+public readonly struct Error;
+
+public readonly struct Error<T>
+{
+    public Error(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; }
+}
